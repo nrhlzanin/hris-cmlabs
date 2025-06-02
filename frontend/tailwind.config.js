ea474019aec212191fac97da3d63c13cc6ee6c6d
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2025-06-02 17:24:07 +0700

    landingpage

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,24 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Ini sudah mencakup semua folder di app/
+    "./src/components/**/*.{js,ts,jsx,tsx}", // kalau kamu punya komponen di luar app
+    "./src/pages/**/*.{js,ts,jsx,tsx}",      // opsional kalau pakai pages router
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Custom colorss
+      colors: {
+        darkgray: "#595959",
+        rosered: "#BA3C54",
+        airgray: "#7CA5BF",
+        berkeley: "#1E3A5F",
+        dodgerblue: "#2D8EFF",
+        milanored: "#C11106",
+        yellowsea: "#FFAB00",
+        jade: "#257047",
+      },
+    },
   },
   plugins: [],
 };
